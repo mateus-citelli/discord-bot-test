2004df49bad487b6e683dd67b5fb934518c5edf2
mateus <112911417+mateus-citelli@users.noreply.github.com>
2025-05-03 21:11:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.typing = True
+
+bot =  commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Nós estamos prontos para começar, {bot.user.name}")
+
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
