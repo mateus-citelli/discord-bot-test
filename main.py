5e8d554c5b264bee976d31b995c22a8a894821a3
mateus <112911417+mateus-citelli@users.noreply.github.com>
2025-05-04 12:50:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,6 +20,27 @@ bot =  commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print("Nós estamos prontos para começar, {bot.user.name}")
 
+@bot.event
+async def on_member_join(member):
+    guild = member.guild
+    role = discord.utils.get(guild.roles, name="Membro")
+    if role not in member.roles:
+        await member.add_roles(role)
+        print(f"Adicionando o cargo {role.name} para {member.name}")
+
+@bot.command()
+async def change_role(ctx, rolename):
+    guild = ctx.guild
+    role = discord.utils.get(guild.roles, name=rolename)
+    if role is None:
+        await ctx.send(f"O cargo `{rolename}` não foi encontrado.")
+        return
+    if role not in ctx.author.roles:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention}, agora você tem o cargo **{role.name}**!")
+    else:
+        await ctx.send(f"{ctx.author.mention}, você já possui o cargo **{role.name}**.")
+
 
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
